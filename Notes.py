56ae3acef31cbce82fd337fd30b462ac8092e672
vishal.khondre@gmail.com <vishal.khondre@gmail.com>
2020-07-28 11:02:48 +0000

    install pre-requsite softwares

---
+++ Notes.py
@@ -0,0 +1,14 @@
+# Install git on Centos
+https://www.digitalocean.com/community/tutorials/how-to-install-git-on-centos-7
+
+# Install wget package
+https://www.cyberciti.biz/faq/yum-install-wget-redhat-cetos-rhel-7/
+
+# Install JDK
+https://www.digitalocean.com/community/tutorials/how-to-install-java-on-centos-and-fedora
+
+# Install maven
+wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar -xvf apache-maven-3.6.3-bin.tar.gz
+mv apache-maven-3.6.3 /opt/maven
+ln -s /opt/maven/bin/mvn /usr/bin/mvn
