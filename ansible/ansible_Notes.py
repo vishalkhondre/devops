2dd706fd19d616d6fb6194643899c64b41d26893
vishal.khondre@gmail.com <vishal.khondre@gmail.com>
2020-07-29 09:58:23 +0000

    first pb

--- ansible/ansible_Notes.py
+++ ansible/ansible_Notes.py
@@ -1,3 +1,15 @@
 """
     Capture all notes related to Ansible
 """
+
+# path of Ansible install files
+cd /etc/ansible/
+
+# copy folder to new location
+cp -Rf source_dir dest_dir
+
+# change the ownership of folder
+chown -Rf vmadmin folder
+
+# change the group of the folder
+chgrp -Rf vmadmin folder
